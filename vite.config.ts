b25d13ba97b28b3081e4f0c4da67d9f024e56f17
Filip Noga <fnoga443@gmail.com>
2023-10-27 20:13:03 +0200

    AAA

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs/promises";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "tsx",
-  },
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".ts": "tsx",
-      },
-    },
-  },
 });
